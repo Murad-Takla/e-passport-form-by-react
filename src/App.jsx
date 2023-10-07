b83b80e2edf9b8ddac87cd2fc7ed0dafdc0eb57d
Murad-Takla <irfanchowdhurycr7@gmail.com>
2023-10-07 17:06:27 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,101 @@
 
-import { useState } from 'react'
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod"
+import {z} from 'zod'
 import './App.css'
+
+const schema = z.object({
+  fullName : z.string().min(2 , { message: "At least 2 character" }).max(30, { message: "Must be 30 or fewer characters long" }),
+  firstName : z.string().max(15, { message: "Must be 30 or fewer characters long" }),
+  lastName : z.string().max(15, { message: "Must be 30 or fewer characters long" }),
+ 
+ 
+
+  religion : z.string().min(2 , { message: "At least 2 character" }).max(30, { message: "Must be 30 or fewer characters long" })  
+})
 
 function App() {
 
+ const {register , handleSubmit  , formState : {errors}} = useForm ({
+  defaultValues: {
+    fullName : "",
+    firstName : "",
+    lastName : "",
+    DOB : "",
+    countryName : "",
+    district : "",
+    maritalStatus: "" ,
+    NID : "",
+    birthRegistrationNo : "",
+    contactNo : "",
+    religion : ""
 
- 
+  },
+  resolver : zodResolver(schema)
+ })
+
+  
   return (
     <>
 
 
 
-      <form className='bg-blue-400 max-w-xl mx-auto mt-10 p-4 rounded text-white space-y-6' 
-      onSubmit={(e)=>{
-        e.defaultPrevented()
-        console.log('something is being added')
-       
-      }}
+      <form className='bg-blue-400 max-w-xl mx-auto mt-10 p-4 rounded text-white space-y-6'
+
+
+     onSubmit={handleSubmit (function (values)
+      {
+        console.log(values)
+      })}
+
       >
 
         <h1 className='text-2xl font-bold text-center text underline p-2'>E-passport form</h1>
         <div>
-          <label htmlFor='full-name' className='font-bold'>Full Name (as per NID/BRC) : </label>
-          <input id='full-name' name='full-name' type="text" className='rounded px-2 text-blue-800' />
+
+          <label htmlFor='fullName' className='font-bold'>Full Name (as per NID/BRC) : </label>
+          <input
+            {...register("fullName")}
+            id='fullName' name='fullName' type="text" className='rounded px-2 text-blue-800' />
+            {errors.fullName && <p className='text-red-800 text-sm'>*{errors.fullName.message}</p>}
         </div>
 
         <div>
-          <label htmlFor='first-name' className='font-bold'>Given Name : </label>
-          <input id='first-name' name='first-name' type="text" className='rounded px-2 text-blue-800' />
+          <label htmlFor='firstName' className='font-bold'>Given Name : </label>
+          <input 
+          {...register("firstName")}
+          id='firstName' name='firstName' type="text" className='rounded px-2 text-blue-800' />
+            {errors.firstName && <p className='text-red-800 text-sm'>*{errors.firstName.message}</p>}
         </div>
 
         <div>
-          <label htmlFor='last-name' className='font-bold'>Surname : </label>
-          <input id='last-name' name='last-name' type="text" className='rounded px-2 text-blue-800' />
+          <label htmlFor='lastName' className='font-bold'>Surname : </label>
+          <input 
+          {...register("lastName")}
+          id='lastName' name='lastName' type="text" className='rounded px-2 text-blue-800' />
+           {errors.lastName && <p className='text-red-800 text-sm'>*{errors.lastName.message}</p>}
         </div>
         <div>
           <label htmlFor='DOB' className='font-bold'>Date of Birth : </label>
-          <input id='DOB' name='DOB' type="date" className='rounded px-2 text-blue-800' />
+          <input
+          {...register("DOB")}
+          id='DOB' name='DOB' type="date" className='rounded px-2 text-blue-800' />
+            {errors.DOB && <p className='text-red-800 text-sm'>*{errors.DOB.message}</p>}
         </div>
         <div>
-          <label htmlFor='country-name' className='font-bold'>Name of country : </label>
-          <input id='country-name' name='country-name' type="text" className='rounded px-2 text-blue-800' />
+          <label htmlFor='countryName' className='font-bold'>Name of country : </label>
+          <input 
+          {...register("countryName")}
+          id='countryName' name='countryName' type="text" className='rounded px-2 text-blue-800' />
+           {errors.countryName && <p className='text-red-800 text-sm'>*{errors.countryName.message}</p>}
         </div>
         <div>
           <label htmlFor="district" className='font-bold '> District of Birth : </label>
-          <select id="district" name="district" className='rounded px-2 text-blue-800' >
+          <select 
+          {...register("district")}
+          id="district" name="district" className='rounded px-2 text-blue-800' >
+               {errors.district && <p className='text-red-800 text-sm'>*{errors.district.message}</p>}
 
             <option value="ctg">CTG</option>
             <option value="dhaka">Dhaka</option>
@@ -57,38 +108,52 @@ function App() {
         </div>
         <div>
           <label htmlFor="maritalStatus" className='font-bold '> Marital Status : </label>
-          <select id="maritalStatus" name="maritalStatus" className='rounded px-2 text-blue-800' >
+          <select
+          {...register("maritalStatus") }
+          id="maritalStatus" name="maritalStatus" className='rounded px-2 text-blue-800' >
+       {errors.maritalStatus && <p className='text-red-800 text-sm'>*{errors.maritalStatus.message}</p>}
 
             <option value="single">SINGLE</option>
             <option value="married">MARRIED</option>
             <option value="widow">WIDOW</option>
             <option value="divorce">DIVORCED</option>
-   
+
           </select>
 
         </div>
         <div>
           <label htmlFor='NID' className='font-bold'>National ID No. </label>
-          <input id='NID' name='NID' type="number" className='rounded px-2 text-blue-800' />
+          <input 
+          {...register("NID" , { required: true })}
+          id='NID' name='NID' type="number" className='rounded px-2 text-blue-800' />
+             {errors.NID && <p className='text-red-800 text-sm'>*{errors.NID.message}</p>}
         </div>
         <div>
           <label htmlFor='birthRegistrationNo' className='font-bold'>Birth Registration No.  </label>
-          <input id='birthRegistrationNo' name='birthRegistrationNo' type="number" className='rounded px-2 text-blue-800' />
+          <input 
+          {...register("birthRegistrationNo" , { required: true })}
+          id='birthRegistrationNo' name='birthRegistrationNo' type="number" className='rounded px-2 text-blue-800' />
+           {errors.birthRegistrationNo && <p className='text-red-800 text-sm'>*{errors.birthRegistrationNo.message}</p>}
         </div>
 
         <div>
           <label htmlFor='contactNo' className='font-bold'>Contact No.  </label>
-          <input id='contactNo' name='contactNo' type="text" className='rounded px-2 text-blue-800' />
+          <input 
+          {...register("contactNo" , { required: true })}
+          id='contactNo' name='contactNo' type="number" className='rounded px-2 text-blue-800' />
+           {errors.contactNo && <p className='text-red-800 text-sm'>*{errors.contactNo.message}</p>}
         </div>
         <div>
           <label htmlFor='religion' className='font-bold'>Religion :   </label>
-          <input id='religion' name='religion' type="text" className='rounded px-2 text-blue-800' />
+          <input 
+          {...register("religion" , { required: true })}
+          id='religion' name='religion' type="text" className='rounded px-2 text-blue-800' />
+            {errors.fullName && <p className='text-red-800 text-sm'>*{errors.fullName.message}</p>}
         </div>
 
         <div className='flex justify-center'>
-        <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" 
-        
-        >SUBMIT</button>
+
+          <button className='bg-cyan-600 p-2 rounded ' > Submit</button>
         </div>
 
 
